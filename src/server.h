c4c37eb566bfcfbe297ecbca1c1c4496331c5beb
Nikhil Dhumal <nikhildhumal2003@gmail.com>
2025-10-30 07:33:28 +0530

    feat(server): initial CivetWeb server setup with /ping endpoint

---
+++ src/server.h
@@ -0,0 +1,12 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include "civetweb.h"
+
+extern struct mg_context *server_ctx;
+
+struct mg_context *start_server(void);
+void stop_server(void);
+int handle_ping(struct mg_connection *conn, void *cbdata);
+
+#endif
